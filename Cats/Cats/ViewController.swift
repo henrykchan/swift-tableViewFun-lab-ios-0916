aba5041b15564a89e1780d876a005700b7990792
henrykchan <chan.henryk@gmail.com>
2016-10-06 22:28:02 -0400

    Cats 💯

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,37 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    var catsArray = ["Garfield", "Clifford", "Meowith", "CoolCat", "DumbCat", "FatCat", "SkinnyCat", "WhatTheCat", "Sabrina"]
     
-   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
     
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return catsArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        
+        let catRow = catsArray[indexPath.row]
+        
+        cell.textLabel?.text = String(catRow)
+        
+        return cell
+        
+    }
 }
